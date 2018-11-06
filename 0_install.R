af3e1843228128ccb0656cb94998287a7a8d85d8
Andrew Conklin <aconklin@origent.com>
2018-11-06 22:17:45 +0000

    updates to markdown

--- 0_install.R
+++ 0_install.R
@@ -6,14 +6,9 @@ package_s3_key <<- "survival-titanic"
 install.packages('dplyr', repos='http://cran.us.r-project.org')
 install.packages('gbm', repos='http://cran.us.r-project.org')
 install.packages('aws.s3', repos='http://cran.us.r-project.org')
-install.packages('reticulate', repos='http://cran.us.r-project.org')
 
 #PYTHON
 system(paste0("pip3 uninstall -y serveRless"))
 system(paste0("rm serveRless-0.0.1.tar.gz"))
 system(paste0("aws s3 cp s3://", package_s3_bucket, "/", package_s3_key, "/serveRless-0.0.1.tar.gz", " serveRless-0.0.1.tar.gz"))
 system(paste0("pip3 install serveRless-0.0.1.tar.gz"))
-
-library(reticulate)
-use_python("/usr/bin/python3", required = TRUE)
-repl_python()
